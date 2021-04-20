966d07e5b1581b57ca1095625e68000fff16a61c
Mian Uddin <mianuddin@gmail.com>
2021-04-20 12:45:40 -0700

    feat: make pkg installable via pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="recommender_pkg",
+                     version="0.0.1",
+                     author="Mian Uddin",
+                     description="A small recommender package",
+                     packages=["recommender_pkg"],
+                     install_requires = ["numpy"])

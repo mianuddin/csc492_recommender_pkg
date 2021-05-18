ea0d71ec8c1905e7354e6f5fc03893638c4866ff
Mian Uddin <mianuddin@gmail.com>
2021-05-18 11:10:37 -0700

    feat: write out evaluation docstrings

--- recommender_pkg/evaluation.py
+++ recommender_pkg/evaluation.py
@@ -10,9 +10,20 @@ from .preprocessing import get_standard_layers
 
 
 def plot_metric_history(history_df, title=""):
-    """Plot each metric versus epochs."""
+    """Plot each metric versus epochs.
+
+    Args:
+        history_df (pandas.DataFrame): A tidy dataframe with `epoch`, `metric`,
+                                       and `value` columns.
+        title (String): Text which will be prepended to the title of each
+                        graph.
+
+    Returns:
+        List[FacetGrid, ...]: The metric plots.
+    """
     metrics = filter(lambda x: not re.match("val", x),
                      history_df["metric"].unique())
+    title_val = title + " " if title != "" else ""
     plots = []
 
     for metric in metrics:
@@ -23,7 +34,7 @@ def plot_metric_history(history_df, title=""):
                                  data=filtered_df,
                                  kind="line",
                                  hue="metric")
-                        .set(title=f"{title} {metric}"))
+                        .set(title=f"{title_val}{metric}"))
 
     return plots
 
@@ -37,7 +48,37 @@ def evaluate_model(ModelConstructor,
                    seed_val,
                    configs,
                    plot=False):
-    """Evaluate multiple of model configs."""
+    """Evaluate multiple of model configs.
+
+    Args:
+        ModelConstructor(KerasRecommender): The constructor of the model which
+                                            is being evaluated.
+        model_name (String): The name of the model which is being evaluated.
+        X_train (ndarray of shape (n_samples, 2)): This is the train set. An
+                                                   array where each row
+                                                   consists of a user and an
+                                                   item.
+        X_test (ndarray of shape (n_samples, 2)): This is the test set. An
+                                                  array where each row consists
+                                                  of a user and an item.
+        y_train (ndarray of shape (n_samples,)): This is the train set. An
+                                                 array where each entry denotes
+                                                 interactions between the
+                                                 corresponding user and item.
+        y_test (ndarray of shape (n_samples,)): This is the train set. An array
+                                                where each entry denotes
+                                                interactions between the
+                                                corresponding user and item.
+        seed_val (int): A random seed.
+        configs (List[Dict[String, Object]]): A list of dictionaries of keyword
+                                             arguments to be applied in the
+                                             model's constructor.
+        plot (bool): Should training plots be made?
+
+    Returns:
+        Dict[String, Dict]: The configs, trained models, history dataframes,
+                            training plots, and groupwise evaluations.
+    """
 
     trained_models = {}
     history_dfs = {}
